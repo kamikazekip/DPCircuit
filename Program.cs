b5c9e009706c89b06151c92e754d7542676ff379
kamikazekip <erik.brandsma@outlook.com>
2015-06-07 13:40:23 +0200

    relative path naar debug

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using Simulatie1.readers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +14,11 @@ namespace Simulatie1
         static void Main(string[] args)
         {
             ReaderFactory readerFactory = new ReaderFactory();
-            Reader reader = readerFactory.createReader(@"C:\Users\Jip\Documents\School\DP1\DPCircuit\bin\Debug\circuit4.txt");
+            string executableLocation = Path.GetDirectoryName(
+             Assembly.GetExecutingAssembly().Location);
+            string circuitLocation = Path.Combine(executableLocation, "circuit1.txt");
+            Console.WriteLine(circuitLocation);
+            Reader reader = readerFactory.createReader(circuitLocation);
             NodeCircuit circuit = new NodeCircuit();
             circuit.startCircuit(reader);
             Console.ReadLine();
